bb20121696799846a26c1c332b1abc29e358c1fb
ChristopherANelson <59150814+ChristopherANelson@users.noreply.github.com>
2025-09-02 11:28:53 -0400

    Creating a level and more assets
    Added an exit and start area, as well as a default level. Created a parallax background

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,15 @@
 extends Node2D
-@onready var start_position = $"Start Position"
-@onready var player = $Player
+@onready var start = $Start
+var player = null
+
+func _ready() -> void:
+	player = get_tree().get_first_node_in_group("player")
+	if player != null:
+		player.global_position = start.get_spawn_pos()
+	var traps = get_tree().get_nodes_in_group("traps")
+	for trap in traps:
+		#trap.connect("touched_player", _on_trap_touched_player)
+		trap.touched_player.connect(_on_trap_touched_player)
 
 
 func _on_deathzone_body_entered(body: Node2D) -> void:
@@ -13,4 +22,4 @@ func _on_trap_touched_player() -> void:
 
 func reset_player():
 	player.velocity = Vector2.ZERO
-	player.global_position = start_position.global_position
+	player.global_position = start.get_spawn_pos()
